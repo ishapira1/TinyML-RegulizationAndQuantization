f6c7db29cba9db6d45ddecec9a9f56ce6c58f705
Itai Shapira <i.j.shapira@gmail.com>
2023-11-03 20:54:52 -0400

    a setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='TinyML-RegulizationAndQuantization',
+    version='0.1.0',
+    author='Itai Shapira',
+    author_email='itaishapira@g.harvard.edu',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        # Add your package dependencies here
+        'torch',
+        'torchvision',
+        # 'numpy', 'matplotlib', etc.
+    ],
+    python_requires='>=3.6',
+)
