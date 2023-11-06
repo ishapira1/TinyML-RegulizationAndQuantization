59f4204bdc7373b74ef0b97effbc23481055837b
Itai Shapira <i.j.shapira@gmail.com>
2023-11-06 18:43:21 -0500

    added time to the logs

--- src/logs/parser.py
+++ src/logs/parser.py
@@ -7,7 +7,23 @@ from logger import Logger, RESULTS_FILE_NAME_IN_LOGS, CHECKPOINT_FILE_NAME_IN_LO
 from torch import load
 import torch
 from tqdm import tqdm
+import datetime
 
+
+def get_model_size_in_mb(model):
+    """
+    Calculate the total size of a PyTorch model in megabytes (MB).
+
+    :param model: The PyTorch model.
+    :return: The total size of the model in MB.
+    """
+    param_size = 0
+    for param in model.parameters():
+        param_size += param.nelement() * param.element_size()
+    buffer_size = sum([buf.nelement() * buf.element_size() for buf in model.buffers()])
+    total_size = param_size + buffer_size
+    total_size_mb = total_size / (1024 ** 2)  # Convert bytes to MB
+    return total_size_mb
 
 
 def calculate_weight_statistics(checkpoint_path):
@@ -15,7 +31,7 @@ def calculate_weight_statistics(checkpoint_path):
     Calculate the statistics of the weights from the model checkpoint.
 
     :param checkpoint_path: Path to the model checkpoint file.
-    :return: A dictionary with total number of weights, L1 norm, L2 norm, max and min weight values.
+    :return: A dictionary with total number of weights, L1 norm, L2 norm, max and min weight values, and model size in MB.
     """
     state_dict = load(checkpoint_path, map_location=torch.device('cpu'))
     # Filter out non-floating point parameters
@@ -27,13 +43,19 @@ def calculate_weight_statistics(checkpoint_path):
     max_weight = max(param.max().item() for param in float_params)
     min_weight = min(param.min().item() for param in float_params)
 
+    # Calculate model size in bytes and convert to megabytes
+    total_size_bytes = sum(param.nelement() * param.element_size() for param in float_params)
+    total_size_mb = total_size_bytes / (1024 ** 2)
+
     return {
         'total_weights': total_weights,
         'l1_norm': l1_norm,
         'l2_norm': l2_norm,
         'max_weight': max_weight,
-        'min_weight': min_weight
+        'min_weight': min_weight,
+        'model_size_mb': total_size_mb
     }
+
 
 def parser():
     # Create a Logger instance to get the log directory
@@ -64,8 +86,10 @@ def parser():
     # Create a DataFrame from the records
     df = pd.DataFrame(all_records)
 
+    timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
     # Save the DataFrame to a CSV file
-    csv_file = os.path.join(logger.log_dir, 'all_results.csv')
+    csv_file = os.path.join(logger.log_dir, f'all_results_{timestamp}.csv')
+
     df.to_csv(csv_file, index=False)
 
 # To run the parser function when the script is executed
