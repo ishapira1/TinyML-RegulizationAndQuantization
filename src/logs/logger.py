097f0c8bd052699d543a8b279780622fbd48b7dc
Itai Shapira <i.j.shapira@gmail.com>
2023-11-06 18:11:22 -0500

    tested logging

--- src/logs/logger.py
+++ src/logs/logger.py
@@ -2,35 +2,50 @@ import os
 import json
 import datetime
 from torch import save
+import os
+import getpass
+
+RESULTS_FILE_NAME_IN_LOGS = 'results.json'
+CHECKPOINT_FILE_NAME_IN_LOGS = 'model_checkpoint.pth'
+
 
 class Logger:
     def __init__(self, log_dir="logs"):
-        self.log_dir = log_dir
-        if not os.path.exists(log_dir):
-            os.makedirs(log_dir)
+        self.script_dir = os.path.dirname(os.path.abspath(__file__))
+        self.log_dir = os.path.join(self.script_dir, 'results')
+        if not os.path.exists(self.log_dir):
+            os.makedirs(self.log_dir)
 
-    def log(self, params, regularization, model, train_loss, test_loss, train_accuracy, test_accuracy):
+    def log(self, model, train_loss, test_loss, model_name, dataset_name, reg_name, param, train_accuracy, test_accuracy, **kwargs):
         # Create a unique identifier for the experiment based on the current timestamp
         timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-        exp_name = f"{params['model_name']}_{params['dataset_name']}_{regularization}_{timestamp}"
+        user = getpass.getuser()
+        exp_name = f"{user}_{model_name}_{dataset_name}_{reg_name}_{param}_{timestamp}"
         exp_dir = os.path.join(self.log_dir, exp_name)
         os.makedirs(exp_dir)
 
         # Save parameters and results
         params_and_results = {
-            'params': params,
-            'regularization': regularization,
+            'runner_id':user,
+            'model_name':model_name,
+            'dataset_name':dataset_name,
+            'regularization': reg_name,
+            'regularization_param': param,
             'train_loss': train_loss,
             'test_loss': test_loss,
             'train_accuracy': train_accuracy,
             'test_accuracy': test_accuracy,
             'timestamp': timestamp
         }
-        with open(os.path.join(exp_dir, 'results.json'), 'w') as f:
+
+        # Update the dictionary with any additional keyword arguments
+        params_and_results.update(kwargs)
+
+        with open(os.path.join(exp_dir, RESULTS_FILE_NAME_IN_LOGS), 'w') as f:
             json.dump(params_and_results, f, indent=4)
 
         # Save the model checkpoint
-        checkpoint_path = os.path.join(exp_dir, 'model_checkpoint.pth')
+        checkpoint_path = os.path.join(exp_dir, CHECKPOINT_FILE_NAME_IN_LOGS)
         save(model.state_dict(), checkpoint_path)
 
     def save_model_weights(self, model, exp_dir):
---
+++ src/logs/parser.py
@@ -0,0 +1,73 @@
+# @ ishapira 20231106
+# parser.py
+import os
+import json
+import pandas as pd
+from logger import Logger, RESULTS_FILE_NAME_IN_LOGS, CHECKPOINT_FILE_NAME_IN_LOGS
+from torch import load
+import torch
+from tqdm import tqdm
+
+
+
+def calculate_weight_statistics(checkpoint_path):
+    """
+    Calculate the statistics of the weights from the model checkpoint.
+
+    :param checkpoint_path: Path to the model checkpoint file.
+    :return: A dictionary with total number of weights, L1 norm, L2 norm, max and min weight values.
+    """
+    state_dict = load(checkpoint_path, map_location=torch.device('cpu'))
+    # Filter out non-floating point parameters
+    float_params = [param for param in state_dict.values() if torch.is_floating_point(param)]
+
+    total_weights = sum(param.numel() for param in float_params)
+    l1_norm = sum(torch.norm(param, 1).item() for param in float_params)
+    l2_norm = sum(torch.norm(param, 2).item() for param in float_params)
+    max_weight = max(param.max().item() for param in float_params)
+    min_weight = min(param.min().item() for param in float_params)
+
+    return {
+        'total_weights': total_weights,
+        'l1_norm': l1_norm,
+        'l2_norm': l2_norm,
+        'max_weight': max_weight,
+        'min_weight': min_weight
+    }
+
+def parser():
+    # Create a Logger instance to get the log directory
+    logger = Logger()
+    log_dir = logger.log_dir
+
+    # List to hold all experiment records
+    all_records = []
+
+    # Iterate over all experiment directories in the log directory with a progress bar
+    for exp_name in tqdm(os.listdir(log_dir), desc='Parsing experiment results'):
+        exp_dir = os.path.join(log_dir, exp_name)
+        results_file = os.path.join(exp_dir, RESULTS_FILE_NAME_IN_LOGS)
+        checkpoint_path = os.path.join(exp_dir, CHECKPOINT_FILE_NAME_IN_LOGS)
+
+        # Check if the checkpoint file exists
+        if os.path.isfile(checkpoint_path):
+            weight_stats = calculate_weight_statistics(checkpoint_path)
+
+            # Check if the results file exists
+            if os.path.isfile(results_file):
+                # Open the results file and load the JSON data
+                with open(results_file, 'r') as f:
+                    record = json.load(f)
+                    record.update(weight_stats)
+                    all_records.append(record)
+
+    # Create a DataFrame from the records
+    df = pd.DataFrame(all_records)
+
+    # Save the DataFrame to a CSV file
+    csv_file = os.path.join(logger.script_dir, 'all_results.csv')
+    df.to_csv(csv_file, index=False)
+
+# To run the parser function when the script is executed
+if __name__ == '__main__':
+    parser()
