c7ac0262fcc20af94b710c8cbe79f5df5e30ce7d
ishapira <i.j.shapira@gmail.com>
2023-11-26 16:50:39 -0500

    quant

--- src/quantization/quantizer.py
+++ src/quantization/quantizer.py
@@ -1,8 +1,11 @@
 import torch
+from .ptq_common import quantize_model, calibrate
+from brevitas.graph.quantize import preprocess_for_quantize
+from torch.utils.data import Subset
 
 
 class Quantizer:
-    def __init__(self, model, dataloaders, criterion, device, quantization_method=None, regularization=None, verbose=True):
+    def __init__(self, model, dataloaders, criterion, device, bit_width=8, regularization=None, verbose=True):
         """
         Initialize the Trainer.
 
@@ -16,24 +19,18 @@ class Quantizer:
         self.dataloaders = dataloaders
         self.criterion = criterion
         self.device = device
-        self.quantization_method = quantization_method if quantization_method else {}
+        self.bit_width = bit_width
         self.regularization = regularization if regularization else {}
         self.verbose = verbose
+        self.quantized_model = None
 
-    def _compute_accuracy(self, dataloader):
-        """
-        Compute the accuracy given the output of the model and the labels.
-
-        :param outputs: The logits output from the model.
-        :param labels: The true labels.
-        :return: The accuracy as a percentage.
-        """
+    def _compute_quantized_accuracy(self, dataloader):
         correct = 0
         total = 0
         with torch.no_grad():
             for inputs, labels in dataloader:
                 inputs, labels = inputs.to(self.device), labels.to(self.device)
-                outputs = self.model(inputs)
+                outputs = self.quantized_model(inputs)
                 _, predicted = torch.max(outputs.data, 1)
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
@@ -41,20 +38,41 @@ class Quantizer:
         return accuracy
     
     def _apply_quantization(self, model):
-        if "dynamic" in self.quantization_method:
-            quantized_model = quantize_dynamic(model, layers_to_quantize=[torch.nn.Linear, torch.nn.Conv2d])
-        elif "static" in self.quantization_method:
-            quantized_model = quantzize_static(model, self.dataloaders['train']) ##FIXME figure out right fusion
-        else:
-            print("Quantization Method Not Recognized. Returning original Model")
-            quantized_model = model
+        # if "dynamic" in self.quantization_method:
+        #     quantized_model = quantize_dynamic(model, layers_to_quantize=[torch.nn.Linear, torch.nn.Conv2d])
+        # elif "static" in self.quantization_method:
+        #     quantized_model = quantzize_static(model, self.dataloaders['train']) ##FIXME figure out right fusion
+        # else:
+        #     print("Quantization Method Not Recognized. Returning original Model")
+        #     quantized_model = model
+        preprocessed_model = preprocess_for_quantize(
+            model,
+            equalize_iters=0,
+            equalize_merge_bias=False
+        )
+
+        quantized_model = quantize_model(
+            preprocessed_model,
+            backend="generic",
+            act_bit_width=self.bit_width,
+            weight_bit_width=self.bit_width,
+            bias_bit_width="int32",
+            scaling_per_output_channel=False,
+            act_quant_percentile=99.9,
+            act_quant_type='asymmetric',
+            scale_factor_type='float32',
+            weight_narrow_range=False
+        )
+        
+        calibrate(self.dataloaders["calibration"], quantized_model, bias_corr=True)
+
         return quantized_model
 
     def quantize(self):
         """
         Quantize model, and compute train/test loss, train/test accuracy
         """
-        self.quantized_model = self._apply_quantization(self.model)
+        self.quantized_model = self._apply_quantization(self.model).to(self.device)
 
         self.quantized_model.eval()
 
@@ -75,8 +93,8 @@ class Quantizer:
         test_loss = test_running_loss / len(self.dataloaders['test'].dataset)
         train_loss = train_running_loss / len(self.dataloaders['train'].dataset)
 
-        train_accuracy = self._compute_accuracy(self.dataloaders['train'])
-        test_accuracy = self._compute_accuracy(self.dataloaders['test'])
+        train_accuracy = self._compute_quantized_accuracy(self.dataloaders['train'])
+        test_accuracy = self._compute_quantized_accuracy(self.dataloaders['test'])
         return self.quantized_model, train_loss, test_loss,train_accuracy, test_accuracy    
 
 
@@ -131,9 +149,3 @@ def quantzize_static(model, dataloader, fuse_modules = None):
     model_int8 = torch.ao.quantization.convert(model_prepared)
 
     return model_int8
-
-def quantize_linear(model):
-    """
-    
-    """
-    return
